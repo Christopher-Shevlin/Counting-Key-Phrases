3aaa6504952bd262ea07b27fd4dc60da37a17d0e
Christopher Shevlin <47715649+Christopher-Shevlin@users.noreply.github.com>
2021-05-11 21:41:56 +0100

    Commit

--- Py - Phrase Freq.py
+++ Py - Phrase Freq.py
@@ -1,17 +1,33 @@
-from nltk.util import ngrams
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.collocations import *
+#Designed to count phrases of words grouped in 2s
 
-
-data = ["test test test test test"]
-
-bigrams = ngrams(data, 2)
-
-bigrams_c = {}
-for b in bigrams:
-    if b not in bigrams_c:
-        bigrams_c[b] = 1
-    else:
-        bigrams_c[b] += 1
+def group_text(text, group_size):
+    """
+    groups a text into text groups set by group_size
+    returns a list of grouped strings
+    """
+    word_list = text.split()
+    group_list = []
+    for k in range(len(word_list)):
+        start = k
+        end = k + group_size
+        group_slice = word_list[start: end]
+        # append only groups of proper length/size
+        if len(group_slice) == group_size:
+            group_list.append(" ".join(group_slice))
+    return group_list
         
-print(bigrams_c)
+
+text = "test this is test a this is a test"
+
+group_size = 2
+group_list = group_text(text, group_size)
+# convert list to set to avoid duplicates
+group_set = set(group_list)
+
+#if wanting to output a text count uncomment the below and remove the group_set print
+#for group in group_set:
+#    count = text.count(group)
+#    sf = "'%s' appears %d times in the text"
+#   print(sf % (group, count))
+
+print(group_set)
